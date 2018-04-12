71a111a00f59f62b1fd186044b0b9d2983665257
Nhat Pham <hoangnhat123456@live.com>
2018-04-11 21:18:11 -0400

    Add some features.

--- main_poisson_reg.py
+++ main_poisson_reg.py
@@ -13,11 +13,15 @@ sample_submission_path = data_path + "/DengAI_Predicting_Disease_Spread_-_Submis
 
 
 # Read data:
-df_train_features = pd.read_csv(data_path + "/dengue_features_train.csv").fillna(0)
+df_train_features = pd.read_csv(data_path + "/dengue_features_train.csv")
 df_train_labels = pd.read_csv(data_path + "/dengue_labels_train.csv")
-df_test_features = pd.read_csv(data_path + "/dengue_features_test.csv").fillna(0)
-features_list = ["ndvi_sw", "station_max_temp_c", "station_min_temp_c", "station_precip_mm", "station_diur_temp_rng_c", "precipitation_amt_mm", "reanalysis_dew_point_temp_k", "reanalysis_air_temp_k", "reanalysis_relative_humidity_percent", "reanalysis_specific_humidity_g_per_kg", "reanalysis_precip_amt_kg_per_m2", "reanalysis_max_air_temp_k", "reanalysis_min_air_temp_k", "reanalysis_avg_temp_k", "reanalysis_tdtr_k"]
+df_test_features = pd.read_csv(data_path + "/dengue_features_test.csv")
+features_list = [ "station_max_temp_c", "station_min_temp_c", "station_precip_mm", "station_diur_temp_rng_c", "precipitation_amt_mm", "reanalysis_dew_point_temp_k", "reanalysis_air_temp_k", "reanalysis_relative_humidity_percent", "reanalysis_specific_humidity_g_per_kg", "reanalysis_precip_amt_kg_per_m2", "reanalysis_max_air_temp_k", "reanalysis_min_air_temp_k", "reanalysis_avg_temp_k", "reanalysis_tdtr_k", "ndvi_se", "ndvi_sw", "ndvi_ne", "ndvi_nw"]
 n_features = len(features_list)
+
+df_train_features = df_train_features.fillna(df_train_features.mean())
+df_test_features = df_test_features.fillna(df_test_features.mean())
+
 
 X_train = df_train_features[features_list].values
 X_test = df_test_features[features_list].values
